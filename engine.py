c4c84c5a68e499c38d1421f97305ad03511816ca
PrismaH <hatve5454@naver.com>
2019-09-04 18:44:32 +0900

    Update engine.py

--- engine.py
+++ engine.py
@@ -145,7 +145,7 @@ class TetrisEngine:
 
     def step(self, action):
         self.anchor = (int(self.anchor[0]), int(self.anchor[1]))
-        self.shape, self.anchor = self.value_action_map[action](self.shape, self.anchor, self.board)
+        self.shape, self.anchor = self.value_action_map[action](int(self.shape), int(self.anchor), int(self.board))
         # Drop each step
         self.shape, self.anchor = soft_drop(self.shape, self.anchor, self.board)
 
